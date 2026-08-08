fae943bf31afeabdda68b012934bba19488ca09f
Akyl Kalilov <kalilovakylbek56@gmail.com>
2026-08-08 21:29:35 +0700

    File with basic loop logic added

---
+++ loop.py
@@ -0,0 +1,12 @@
+# Some basic loop logic
+
+start = 0
+finish = 100
+step = 4
+for i in range(start, finish + step, step):
+    print(i)
+
+value = 0
+while (value <= finish):
+    print(f"Count number {value}")
+    value += 1

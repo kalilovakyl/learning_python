e99f110bede97b50959834546e4792c75c44b5e3
Akyl Kalilov <kalilovakylbek56@gmail.com>
2026-08-08 21:29:10 +0700

    File with basic if logic added

---
+++ if.py
@@ -0,0 +1,10 @@
+# Some basic if statement logic 
+limit = 50
+user_value = int(input("Enter any value: "))
+if (user_value > limit):
+    print("This value greater than my limit")
+elif (user_value == limit): 
+    print("This value equal to the limit")
+else: 
+    print("This value less than my limit")
+
